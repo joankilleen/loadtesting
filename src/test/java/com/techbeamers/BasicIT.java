aeff679610566347c335f78a8440efad657e0d52
Joan Killeen <joan.killeen@bluewin.ch>
2020-12-29 15:01:36 +0000

    Added a driver factory

--- src/test/java/com/techbeamers/BasicIT.java
+++ src/test/java/com/techbeamers/BasicIT.java
@@ -3,47 +3,49 @@ package com.techbeamers;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
-public class BasicIT {
+public class BasicIT extends DriverBase {
 
-    private ExpectedCondition<Boolean> pageTitleStartsWith(final String searchString) {
-        return driver -> driver.getTitle().toLowerCase().startsWith(searchString.toLowerCase());
-    }
+	private ExpectedCondition<Boolean> pageTitleStartsWith(final String searchString) {
+		return driver -> driver.getTitle().toLowerCase().startsWith(searchString.toLowerCase());
+	}
 
-    private void googleExampleThatSearchesFor(final String searchString) {
+	private void googleExampleThatSearchesFor(final String searchString) {
 
-        WebDriver driver = new FirefoxDriver();
+		WebDriver driver = DriverBase.getDriver();
 
-        driver.get("http://www.google.com");
+		driver.get("http://www.google.com");
 
-        WebElement searchField = driver.findElement(By.name("q"));
+		WebElement searchField = driver.findElement(By.name("q"));
 
-        searchField.clear();
-        searchField.sendKeys(searchString);
+		searchField.clear();
+		searchField.sendKeys(searchString);
 
-        System.out.println("Page title is: " + driver.getTitle());
+		System.out.println("Page title is: " + driver.getTitle());
 
-        searchField.submit();
+		searchField.submit();
 
-        WebDriverWait wait = new WebDriverWait(driver, 10, 100);
-        wait.until(pageTitleStartsWith(searchString));
+		WebDriverWait wait = new WebDriverWait(driver, 10, 100);
+		wait.until(pageTitleStartsWith(searchString));
 
-        System.out.println("Page title is: " + driver.getTitle());
+		System.out.println("Page title is: " + driver.getTitle());
+	}
 
-        driver.quit();
-    }
+	@Test
+	public void googleMilkExample() {
+		googleExampleThatSearchesFor("Milk!");
+	}
 
-    @Test
-    public void googleCheeseExample() {
-        googleExampleThatSearchesFor("Cheese!");
-    }
+	@Test
+	public void googleCheeseExample() {
+		googleExampleThatSearchesFor("Cheese!");
+	}
 
-    @Test
-    public void googleMilkExample() {
-        googleExampleThatSearchesFor("Milk!");
-    }
+	@Test
+	public void googleJohnGrisham() {
+		googleExampleThatSearchesFor("John Grisham");
+	}
 }
