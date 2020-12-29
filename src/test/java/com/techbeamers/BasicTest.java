4e9fae008c65b38f7b83f19d8b46c22397fba4cc
Joan Killeen <joan.killeen@bluewin.ch>
2020-12-29 13:02:28 +0000

    maven failsafe plugin

--- src/test/java/com/techbeamers/BasicTest.java
+++ src/test/java/com/techbeamers/BasicTest.java
@@ -10,40 +10,40 @@ import org.testng.annotations.Test;
 
 public class BasicTest {
 
-	private ExpectedCondition<Boolean> pageTitleStartsWith(final String searchString) {
-		return driver -> driver.getTitle().toLowerCase().startsWith(searchString.toLowerCase());
-	}
+    private ExpectedCondition<Boolean> pageTitleStartsWith(final String searchString) {
+        return driver -> driver.getTitle().toLowerCase().startsWith(searchString.toLowerCase());
+    }
 
-	private void googleExampleThatSearchesFor(final String searchString) {
+    private void googleExampleThatSearchesFor(final String searchString) {
 
-		WebDriver driver = new FirefoxDriver();
+        WebDriver driver = new FirefoxDriver();
 
-		driver.get("http://www.google.com");
+        driver.get("http://www.google.com");
 
-		WebElement searchField = driver.findElement(By.name("q"));
+        WebElement searchField = driver.findElement(By.name("q"));
 
-		searchField.clear();
-		searchField.sendKeys(searchString);
+        searchField.clear();
+        searchField.sendKeys(searchString);
 
-		System.out.println("Page title is: " + driver.getTitle());
+        System.out.println("Page title is: " + driver.getTitle());
 
-		searchField.submit();
+        searchField.submit();
 
-		WebDriverWait wait = new WebDriverWait(driver, 10, 100);
-		wait.until(pageTitleStartsWith(searchString));
+        WebDriverWait wait = new WebDriverWait(driver, 10, 100);
+        wait.until(pageTitleStartsWith(searchString));
 
-		System.out.println("Page title is: " + driver.getTitle());
+        System.out.println("Page title is: " + driver.getTitle());
 
-		driver.quit();
-	}
+        driver.quit();
+    }
 
-	@Test
-	public void googleCheeseExample() {
-		googleExampleThatSearchesFor("Cheese!");
-	}
+    @Test
+    public void googleCheeseExample() {
+        googleExampleThatSearchesFor("Cheese!");
+    }
 
-	@Test
-	public void googleMilkExample() {
-		googleExampleThatSearchesFor("Milk!");
-	}
+    @Test
+    public void googleMilkExample() {
+        googleExampleThatSearchesFor("Milk!");
+    }
 }
